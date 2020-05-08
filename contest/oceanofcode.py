9caa500259cadfbd129c36cb310ae2367908ef9a
nschweitzer <nicolas.schweitzer@amadeus.com>
2020-05-08 22:17:40 +0200

    oceanofcode bronze

--- contest/oceanofcode.py
+++ contest/oceanofcode.py
@@ -1,10 +1,9 @@
 import sys
-import random
 import collections
+import logging
+import heapq
 
-
-def d(*x):
-    print(*x, file=sys.stderr)
+d = logging.debug
 
 
 class Queue:
@@ -21,39 +20,49 @@ class Queue:
         return self.elements.popleft()
 
 
-class Point:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
 
-    def __str__(self):
-        return f'[{self.x}, {self.y}]'
+    def empty(self):
+        return len(self.elements) == 0
 
-    def __repr__(self):
-        return f'[{self.x}, {self.y}]'
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
+    def get(self):
+        return heapq.heappop(self.elements)[1]
 
 
-class Player(Point):
-    def __init__(self, x=0, y=0, idx=0):
-        Point.__init__(self, x, y)
-        self.idx = idx
-        self.life = 0
-        self.torpedo = 0
-        self.sonar = 0
-        self.silence = 0
-        self.mine = 0
+Point = collections.namedtuple('Point', ['x', 'y'], defaults=2 * [0])
+Player = collections.namedtuple('Player', ['idx', 'x', 'y', 'life', 'torpedo', 'sonar', 'silence', 'mine'])
 
-    def __str__(self):
-        return f'#{self.idx}[{self.x}, {self.y}]'
 
-    def __repr__(self):
-        return self.__str__()
+def add_point(p1, p2):
+    return Point(p1.x + p2.x, p1.y + p2.y)
+
+
+def to_point(s):
+    if s == 'N' or s == '':
+        return Point(0, -1)
+    elif s == 'S':
+        return Point(0, 1)
+    elif s == 'E':
+        return Point(1, 0)
+    elif s == 'W':
+        return Point(-1, 0)
+    else:
+        raise ValueError(f'{s} invalid direction')
+
+
+def avg_point(points):
+    dx = sum([p.x for p in points]) // len(points)
+    dy = sum([p.y for p in points]) // len(points)
+    return Point(dx, dy)
+
+
+def distance(p1, p2):
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 
 class Map:
@@ -61,12 +70,20 @@ class Map:
         self.w = w
         self.h = h
         self.islands = []
-        self.path = []
-        self.moi, self.lui = Player(), Player()
+        self.my_path = []
+        self.moi, self.his_life = None, 0
+        self.idx, self.dx, self.dy = 3 * [0]
+        self.dn, self.ds, self.de, self.dw = 4 * [0]
+        self.target = None
+        self.his_path = []
+        self.bomb = None
+        self.origin = None
 
     def read_input(self):
-        self.w, self.h, self.moi.idx = [int(i) for i in input().split()]
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(message)s')
+        self.w, self.h, self.idx = [int(i) for i in input().split()]
         d(f'w {self.w}, h {self.h}')
+        d(self.moi)
         for y in range(self.h):
             line = input()
             for x in range(self.w):
@@ -74,68 +91,233 @@ class Map:
                     self.islands.append(Point(x, y))
         d(f'{len(self.islands)} islands')
 
+    def surface_voisine(self):
+        n = self.surface(self.target)
+        dico = {self.surface_center(x): distance(self.target, self.surface_center(x)) for x in
+             list(range(1, n)) + list(range(n + 1, 10))}
+        return min(dico, key=dico.get)
+
     def turn_input(self):
         the_input = [int(i) for i in input().split()]
-        self.moi.x, self.moi.y = the_input[0:2]
-        self.moi.life, self.lui.life = the_input[2:4]
-        self.moi.torpedo, self.moi.sonar, self.moi.silence, self.moi.mine = the_input[4:]
-        d(f'path is {len(self.path)}')
+        moi = [self.idx] + the_input[0:3] + the_input[4:]
+        self.moi = Player(*moi)
+        self.his_life = the_input[3]
+        d(f'path is {len(self.my_path)}')
         d(self.moi)
-        d(self.lui)
         sonar_result = input()
+        d(sonar_result)
+        if sonar_result == 'N':
+            self.target = self.surface_voisine()
+            self.dn, self.ds, self.de, self.dw = 0, 0, 0, 0
         opponent_orders = input()
+        d(opponent_orders)
+        self.read_orders(opponent_orders)
+        # if self.target and distance(self.moi, self.target) < 4:
+        #     self.bomb = self.target
+        #     self.target = None
+        d(f'dx:{self.dx}, dy{self.dy}, dn{self.dn}, ds{self.ds}')
+        d(f'initial target:{self.target}')
+        self.target = self.guess_target()
+        d(f'guessed target:{self.target}')
+        if self.target is not None:
+            if self.origin is not None:
+                self.target = self.assess_target()
+                d(f'fixed-up: {self.target}')
+        if self.bomb is not None:
+            self.target = self.assess_target(from_bomb=True)
+            d(f'bomb target:{self.target}')
+
+    def guess_target(self):
+        pn, ps, pe, pw = 4 * [{1, 2, 3, 4, 5, 6, 7, 8, 9}]
+        worth = self.dn > 4 or self.ds > 4 or self.de > 4 or self.dw > 4
+        if self.dn > 9:
+            pn = {7, 8, 9}
+        elif self.dn > 4:
+            pn = {4, 5, 6, 7, 8, 9}
+        if self.ds > 9:
+            ps = {1, 2, 3}
+        elif self.ds > 4:
+            ps = {1, 2, 3, 4, 5, 6}
+        if self.de > 9:
+            pe = {1, 4, 7}
+        elif self.de > 4:
+            pe = {1, 2, 4, 5, 7, 8}
+        if self.dw > 9:
+            pw = {3, 6, 9}
+        elif self.dw > 4:
+            pw = {2, 3, 5, 6, 8, 9}
+        list_potential = [self.surface_center(x) for x in list(pn & ps & pe & pw) if
+                          self.valid_path(self.surface_center(x))]
+        if len(list_potential) > 0 and worth:
+            start = avg_point(list_potential)
+            for n in self.his_path:
+                if not self.in_bounds(add_point(start, to_point(n))):
+                    break
+                start = add_point(start, to_point(n))
+            return start
+        return self.target
+
+    def assess_target(self, from_bomb=False):
+        c = []
+        from_point = self.surface(self.bomb) if from_bomb else self.origin
+        for p in self.surface_points(from_point):
+            if self.valid_path(p, reverse=from_bomb):
+                c.append(p)
+        if len(c) > 0:
+            start = avg_point(c)
+            if not from_bomb:
+                for n in self.his_path:
+                    if not self.in_bounds(add_point(start, to_point(n))):
+                        break
+                    start = add_point(start, to_point(n))
+            # return avg_point([start, self.surface_center(from_point)])
+            return start
+        else:
+            return self.target
+
+    def read_orders(self, orders):
+        self.bomb = None
+        for order in orders.split('|'):
+            if 'MOVE' in order:
+                _, where = order.split()
+                self.his_path.append(where)
+                if self.target is not None and self.in_bounds(add_point(self.target, to_point(where))):
+                    self.target = add_point(self.target, to_point(where))
+                if where == 'N':
+                    self.dx -= 1
+                    self.dn += 1
+                    self.dn = min(self.dn, self.h)
+                elif where == 'S':
+                    self.dx += 1
+                    self.ds += 1
+                    self.ds = min(self.ds, self.h)
+                elif where == 'W':
+                    self.dy -= 1
+                    self.dw += 1
+                    self.dw = min(self.dw, self.w)
+                elif where == 'E':
+                    self.dy += 1
+                    self.de += 1
+                    self.de = min(self.de, self.w)
+            elif 'SURFACE' in order:
+                _, target = order.split()
+                self.target = self.surface_center(int(target))
+                self.origin = int(target)
+                self.his_path = []
+                self.dx, self.dy = 0, 0
+            elif 'TORPEDO' in order:
+                _, x, y = order.split()
+                self.bomb = Point(int(x), int(y))
+            elif 'SONAR' in order:
+                pass
+                # _, which = order.split()
+                # self.bomb = self.surface_center(which)
+            elif 'SILENCE' in order:
+                previous = self.his_path[-1] if len(self.his_path) > 0 else 'N'
+                self.his_path += 2 * [previous]
+                if self.target is not None and self.in_bounds(add_point(self.target, to_point(previous))):
+                    self.target = add_point(self.target, to_point(previous))
+            elif 'NA' in order:
+                pass
+            else:
+                raise ValueError(f'Unexpected order {order}')
 
     def in_bounds(self, p):
         return 0 <= p.x < self.w and 0 <= p.y < self.h
 
     def passable(self, p):
-        return p not in self.islands and p not in self.path
+        return p not in self.islands and p not in self.my_path
 
     @staticmethod
     def surface(p):
-        if (p.x // 5) == 0 and (p.y // 5) == 0:
-            return 1
-        elif (p.x // 5) == 1 and (p.y // 5) == 0:
-            return 2
-        elif (p.x // 5) == 2 and (p.y // 5) == 0:
-            return 3
-        elif (p.x // 5) == 0 and (p.y // 5) == 1:
-            return 4
-        elif (p.x // 5) == 1 and (p.y // 5) == 1:
-            return 5
-        elif (p.x // 5) == 2 and (p.y // 5) == 1:
-            return 6
-        elif (p.x // 5) == 0 and (p.y // 5) == 2:
-            return 7
-        elif (p.x // 5) == 1 and (p.y // 5) == 2:
-            return 8
-        elif (p.x // 5) == 2 and (p.y // 5) == 2:
-            return 9
-        else:
+        s = 3 * (p.y // 5) + (p.x // 5) + 1
+        if s < 1 or s > 9:
             raise ValueError(f'{p} is out of bounds')
+        return s
+
+    @staticmethod
+    def surface_center(n):
+        return Point(5 * ((n - 1) % 3) + 2, 5 * ((n - 1) // 3) + 2)
+
+    def surface_points(self, n):
+        points = []
+        for i in range(5):
+            for j in range(5):
+                p = Point(5 * ((n - 1) % 3) + i, 5 * ((n - 1) // 3) + j)
+                if p not in self.islands:
+                    points.append(p)
+        return points
 
     def torpedo_range(self, p):
         r = []
         t = 4
         angles = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
-        for a in range(len(angles)):  # les 4 angles
+        for a in range(len(angles)):
             angle = angles[a]
-            for x in range(t):  #
+            for x in range(t):
                 for y in range(t - x, 0, -1):
-                    dx = angle[0] * x if a // 2 else angle[0] * y
-                    dy = angle[1] * y if a // 2 else angle[1] * x
+                    dx = angle[0] * x if a % 2 else angle[0] * y
+                    dy = angle[1] * y if a % 2 else angle[1] * x
                     z = Point(p.x + dx, p.y + dy)
                     if z not in self.islands:
                         r.append(z)
         return r
 
-    @staticmethod
-    def torpedo_impact(p):
+    def torpedo_impact(self, p):
         r = []
         for x in range(-1, 2):
             for y in range(-1, 2):
-                r.append(Point(p.x + x, p.y + y))
+                n = Point(p.x + x, p.y + y)
+                if self.in_bounds(n):
+                    r.append(n)
         return r
+
+    @staticmethod
+    def heuristic(a, b):
+        return abs(a.x - b.x) + abs(a.y - b.y)
+
+    def a_star(self, start, goal, from_start=True):
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from, cost = {}, {}
+        came_from[start] = None
+        cost[start] = 0
+        while not frontier.empty():
+            current = frontier.get()
+            if current == goal:
+                break
+            for n in self.neighbors(current):
+                new_cost = cost[current] + 1
+                if n not in cost or new_cost < cost[n]:
+                    cost[n] = new_cost
+                    priority = new_cost + self.heuristic(goal, n)
+                    frontier.put(n, priority)
+                    came_from[n] = current
+        # rebuild path
+        current = goal
+        path = []
+        if goal in came_from:
+            while current != start:
+                path.append(current)
+                current = came_from[current]
+            # path.append(start)  # optional
+            if from_start:
+                path.reverse()  # optional
+        return path
+
+    def bfs(self, start, filt=False):
+        frontier = Queue()
+        frontier.put(start)
+        visited = {start: None}
+        while not frontier.empty():
+            current = frontier.get()
+            for n in self.neighbors(current, filt=filt):
+                if filt and self.passable(n):
+                    return n
+                if n not in visited:
+                    frontier.put(n)
+                    visited[n] = current
+        return start if filt else visited
 
     def neighbors(self, p, filt=True):
         n = [Point(p.x, p.y - 1), Point(p.x, p.y + 1), Point(p.x - 1, p.y), Point(p.x + 1, p.y)]
@@ -147,54 +329,105 @@ class Map:
 
     @staticmethod
     def direction(origin, destination):
-        move = destination - origin
-        if move.x == 0 and move.y == 1:
-            return 'S'
-        elif move.x == 0 and move.y == -1:
-            return 'N'
-        elif move.x == 1 and move.y == 0:
-            return 'E'
-        elif move.x == -1 and move.y == 0:
-            return 'W'
+        move = Point(destination.x - origin.x, destination.y - origin.y)
+        if abs(move.y) > abs(move.x):
+            return 'S' if move.y > 0 else 'N'
         else:
-            raise ValueError(f'{move} is wrong, from {origin} to {destination}')
+            return 'E' if move.x > 0 else 'W'
 
-    def next_flood_fill(self, p):
-        n = self.neighbors(p)
-        return self.direction(p, n[0]) if n else p
+    def valid_path(self, p, reverse=False):
+        c, v = p, 0
+        path = reversed(self.his_path) if reverse else self.his_path
+        for n in path:
+            c = add_point(to_point(n), c)
+            if not self.in_bounds(c):
+                return False
+            if c in self.islands:
+                v += 1
+            if v > 1:
+                return False
+        return True
 
-    def print_position(self):
-        r = random.randint(0, len(self.islands))
-        start = (self.islands[r].x, self.islands[r].y)
-        frontier = Queue()
-        frontier.put(start)
-        visited = {}
-        visited[start] = True
-        while not frontier.empty():
-            current = frontier.get()
-            for n in self.neighbors(Point(current[0], current[1]), filt=False):
-                t = (n.x, n.y)
-                if self.passable(n):
-                    print(f'{n.x} {n.y}')
-                    return
-                if t not in visited:
-                    frontier.put(t)
-                    visited[t] = True
-        print(f'{start[0]} {start[1]}')
+    def flood_fill(self):
+        n = self.neighbors(self.moi)
+        self.islands.append(Point(self.moi.x, self.moi.y))
+        dico = {x: len(self.bfs(x)) for x in n}
+        del self.islands[-1]
+        return self.direction(self.moi, max(dico, key=dico.get)) if len(dico) > 0 else ''
+
+    def next_move(self):
+        path = []
+        if self.target:
+            if self.target in self.islands:
+                self.target = self.bfs(self.target, filt=True)
+                d(f'shiflands {self.target}')
+            path = self.a_star(self.moi, self.target)
+        return self.direction(self.moi, path[0]) if path else self.flood_fill()
+
+    def can_shoot(self, dest):
+        if self.target is not None:
+            d(f'SHOOT? {distance(self.moi, self.target)}, {self.moi.torpedo}')
+        if self.target is not None and distance(self.moi, self.target) < 5 and self.moi.torpedo == 0:
+            next_point = add_point(self.moi, to_point(dest))
+            if next_point in self.torpedo_impact(self.target):
+                self.target = self.shift(next_point)
+                d(f'shifted')
+            return f'| TORPEDO {self.target.x} {self.target.y}'
+        return ''
+
+    def shift(self, c):
+        for p in self.torpedo_impact(self.target):
+            if c not in self.torpedo_impact(p):
+                return p
+        d = self.direction(self.moi, self.target)
+        t = add_point(self.target, to_point(d))
+        t = add_point(t, to_point(d))
+        return t
+
+    def place_submarine(self):
+        r = len(self.islands) // 2
+        start = self.islands[r]
+        place = self.bfs(start, filt=True)
+        print(f'{place.x} {place.y}')
+
+    def what_charge(self):
+        if self.moi.silence > 0:
+            return 'SILENCE'
+        elif self.moi.torpedo > 0:
+            return 'TORPEDO'
+        elif self.moi.sonar > 0:
+            return 'SONAR'
+        return ''
+
+    def can_sonar(self):
+        if self.target is not None and self.moi.sonar == 0 and self.moi.life <= self.his_life:
+            return f'| SONAR {self.surface(self.target)}'
+        return ''
 
     def play(self):
-        self.path.append(Point(self.moi.x, self.moi.y))
-        dest = self.next_flood_fill(self.moi)
-        if type(dest) is str:
-            print(f'MOVE {dest} TORPEDO')
+        self.my_path.append(Point(self.moi.x, self.moi.y))
+        charge = self.what_charge()
+        dest = self.next_move()
+        shoot = self.can_shoot(dest)
+        sonar = self.can_sonar()
+        if dest:
+            if self.moi.silence == 0 and self.moi.sonar == 0 and self.moi.torpedo == 0:
+                print(f'SILENCE {dest} 1 {shoot} {sonar}')
+            else:
+                print(f'MOVE {dest} {charge} {shoot} {sonar}')
         else:
-            print('SURFACE')
-            self.path = []
+            print(f'SURFACE {shoot} {sonar}')
+            self.my_path = []
 
 
-m = Map()
-m.read_input()
-m.print_position()
-while True:
-    m.turn_input()
-    m.play()
+def main():
+    m = Map()
+    m.read_input()
+    m.place_submarine()
+    while True:
+        m.turn_input()
+        m.play()
+
+
+if __name__ == "__main__":
+    main()
--- contest/test_oceanofcode.py
+++ contest/test_oceanofcode.py
@@ -2,39 +2,95 @@ import unittest
 import oceanofcode
 
 
-class TestTorpedoiRange(unittest.TestCase):
-    def test_range(self):
-        p = oceanofcode.Point(0, 0)
-        m = oceanofcode.Map()
-        r = m.torpedo_range(p)
-        self.assertEqual(len(r), 40)
-        self.assertIn(oceanofcode.Point(-4, 0), r)
-        self.assertIn(oceanofcode.Point(3, 0), r)
-        self.assertIn(oceanofcode.Point(0, 2), r)
-        self.assertIn(oceanofcode.Point(0, -1), r)
+class TestPointsMethods(unittest.TestCase):
+    def test_avg_2(self):
+        a = oceanofcode.avg_point([oceanofcode.Point(0, 0), oceanofcode.Point(0, 2)])
+        self.assertEqual(a, oceanofcode.Point(0, 1))
 
-    def test_islands(self):
-        p = oceanofcode.Point(0, 0)
-        m = oceanofcode.Map()
-        m.islands.append(oceanofcode.Point(1, 1))
-        r = m.torpedo_range(p)
-        self.assertEqual(len(r), 39)
-        self.assertIn(oceanofcode.Point(-3, 0), r)
-        self.assertIn(oceanofcode.Point(3, 1), r)
-        self.assertIn(oceanofcode.Point(2, 2), r)
-        self.assertIn(oceanofcode.Point(1, -1), r)
-        self.assertNotIn(oceanofcode.Point(1, 1), r)
+    def test_avg_4(self):
+        a = oceanofcode.avg_point(
+            [oceanofcode.Point(0, 0), oceanofcode.Point(0, 5), oceanofcode.Point(5, 0), oceanofcode.Point(5, 5)])
+        self.assertEqual(a, oceanofcode.Point(2, 2))
+
+    def test_add_points(self):
+        p1 = oceanofcode.Point(3, 4)
+        p2 = oceanofcode.Point(-4, 6)
+        self.assertEqual(oceanofcode.add_point(p1, p2), oceanofcode.Point(-1, 10))
 
 
-class TestTorpedoImpact(unittest.TestCase):
-    def test_impact(self):
-        p = oceanofcode.Point(0, 0)
-        m = oceanofcode.Map()
-        r = m.torpedo_impact(p)
-        self.assertEqual(len(r), 9)
-        self.assertIn(oceanofcode.Point(-1, -1), r)
-        self.assertIn(oceanofcode.Point(0, 0), r)
-        self.assertNotIn(oceanofcode.Point(2, 1), r)
+class TestGuessTarget(unittest.TestCase):
+    m = oceanofcode.Map(15, 15)
+
+    def test_small_move(self):
+        self.m.dn, self.m.ds, self.m.de, self.m.dw = 0, 4, 0, 0
+        self.m.islands = []
+        self.assertIsNone(self.m.guess_target())
+
+    def test_medium_move(self):
+        self.m.dn, self.m.ds, self.m.de, self.m.dw = 0, 0, 6, 0
+        self.m.islands = []
+        self.assertEqual(self.m.guess_target(), oceanofcode.Point(4, 7))
+
+    def test_big_move(self):
+        self.m.dn, self.m.ds, self.m.de, self.m.dw = 11, 0, 0, 10
+        self.m.islands = []
+        self.assertEqual(self.m.guess_target(), oceanofcode.Point(12, 12))
+
+    def test_with_path(self):
+        self.m.dn, self.m.ds, self.m.de, self.m.dw = 0, 11, 5, 0
+        self.m.islands = []
+        self.m.his_path = 11 * ['S'] + 5 * ['E']
+        self.assertEqual(self.m.guess_target(), oceanofcode.Point(9, 13))
+
+    def test_with_islands(self):
+        self.m.dn, self.m.ds, self.m.de, self.m.dw = 0, 11, 5, 0
+        self.m.islands = [oceanofcode.Point(7, 7), oceanofcode.Point(7, 8)]
+        self.m.his_path = 11 * ['S'] + 5 * ['E']
+        self.assertEqual(self.m.guess_target(), oceanofcode.Point(7, 13))
+
+
+class TestReadOrders(unittest.TestCase):
+    m = oceanofcode.Map()
+
+    def test_move_only(self):
+        self.m.read_orders('MOVE S')
+        self.assertEqual(self.m.dx, 1)
+
+    def test_wrong_move(self):
+        with self.assertRaises(ValueError):
+            self.m.read_orders('MOVE N TORPEDO')
+
+    def test_surface(self):
+        self.m.read_orders('SURFACE 6')
+        self.assertEqual(self.m.target, oceanofcode.Point(12, 7))
+        self.assertEqual(self.m.dx, 0)
+
+    def test_torpedo(self):
+        self.m.read_orders('TORPEDO 6 4')
+        self.assertEqual(self.m.bomb.x, 6)
+        self.assertEqual(self.m.bomb.y, 4)
+
+    def test_sonar(self):
+        self.m.read_orders('SONAR 9')
+        self.assertIsNone(self.m.bomb)
+
+    def test_silence(self):
+        self.m.his_path = ['N']
+        self.m.read_orders('SILENCE')
+        self.assertEqual(len(self.m.his_path), 3)
+        self.assertEqual(self.m.his_path[-1], 'N')
+
+
+class TestInBounds(unittest.TestCase):
+    m = oceanofcode.Map(3, 3)
+
+    def test_in(self):
+        p = oceanofcode.Point(1, 2)
+        self.assertTrue(self.m.in_bounds(p))
+
+    def test_out(self):
+        p = oceanofcode.Point(3, 2)
+        self.assertFalse(self.m.in_bounds(p))
 
 
 class TestSurface(unittest.TestCase):
@@ -70,6 +126,112 @@ class TestSurface(unittest.TestCase):
     def test_10(self):
         with self.assertRaises(ValueError):
             self.m.surface(oceanofcode.Point(-1, 0))
+
+
+class TestSurfaceCenter(unittest.TestCase):
+    m = oceanofcode.Map()
+
+    def test_1(self):
+        p = self.m.surface_center(1)
+        self.assertEqual(p, oceanofcode.Point(2, 2))
+
+    def test_2(self):
+        p = self.m.surface_center(2)
+        self.assertEqual(p, oceanofcode.Point(7, 2))
+
+    def test_3(self):
+        p = self.m.surface_center(3)
+        self.assertEqual(p, oceanofcode.Point(12, 2))
+
+    def test_4(self):
+        p = self.m.surface_center(4)
+        self.assertEqual(p, oceanofcode.Point(2, 7))
+
+    def test_5(self):
+        p = self.m.surface_center(5)
+        self.assertEqual(p, oceanofcode.Point(7, 7))
+
+    def test_6(self):
+        p = self.m.surface_center(6)
+        self.assertEqual(p, oceanofcode.Point(12, 7))
+
+    def test_7(self):
+        p = self.m.surface_center(7)
+        self.assertEqual(p, oceanofcode.Point(2, 12))
+
+    def test_8(self):
+        p = self.m.surface_center(8)
+        self.assertEqual(p, oceanofcode.Point(7, 12))
+
+    def test_9(self):
+        p = self.m.surface_center(9)
+        self.assertEqual(p, oceanofcode.Point(12, 12))
+
+
+class TestTorpedoRange(unittest.TestCase):
+    p = oceanofcode.Point(0, 0)
+    m = oceanofcode.Map()
+
+    def test_islands(self):
+        self.m.islands.append(oceanofcode.Point(1, 1))
+        r = self.m.torpedo_range(self.p)
+        self.assertEqual(len(r), 39)
+        self.assertIn(oceanofcode.Point(-3, 0), r)
+        self.assertIn(oceanofcode.Point(3, 1), r)
+        self.assertIn(oceanofcode.Point(2, 2), r)
+        self.assertIn(oceanofcode.Point(1, -1), r)
+        self.assertNotIn(oceanofcode.Point(1, 1), r)
+
+    def test_range(self):
+        self.m.islands = []
+        r = self.m.torpedo_range(self.p)
+        self.assertEqual(len(r), 40)
+        self.assertIn(oceanofcode.Point(-4, 0), r)
+        self.assertIn(oceanofcode.Point(3, 0), r)
+        self.assertIn(oceanofcode.Point(0, 2), r)
+        self.assertIn(oceanofcode.Point(0, -1), r)
+
+
+class TestTorpedoImpact(unittest.TestCase):
+    m = oceanofcode.Map(5,5)
+
+    def test_nominal_impact(self):
+        r = self.m.torpedo_impact(oceanofcode.Point(2,2))
+        self.assertEqual(len(r), 9)
+        self.assertIn(oceanofcode.Point(1, 1), r)
+        self.assertIn(oceanofcode.Point(2, 2), r)
+        self.assertNotIn(oceanofcode.Point(4, 1), r)
+
+    def test_bound_impact(self):
+        r = self.m.torpedo_impact(oceanofcode.Point(0,0))
+        self.assertEqual(len(r), 4)
+
+
+class TestShift(unittest.TestCase):
+    m = oceanofcode.Map(15, 15)
+
+    def test_simple(self):
+        self.m.moi = oceanofcode.Player(0, 4, 7, 6, 0, 4, 0, -1)
+        self.m.target = oceanofcode.Point(5, 5)
+        self.assertEqual(self.m.shift(oceanofcode.Point(4, 6)), oceanofcode.Point(4, 4))
+    def test_on_me(self):
+        self.m.moi = oceanofcode.Player(0, 8, 4, 6, 0, 4, 0, -1)
+        self.m.target = oceanofcode.Point(9, 4)
+        self.assertEqual(self.m.shift(oceanofcode.Point(9, 4)), oceanofcode.Point(11, 4))
+
+
+class TestAStar(unittest.TestCase):
+    m = oceanofcode.Map(w=15, h=15)
+
+    def test_dist(self):
+        self.m.islands = []
+        path = self.m.a_star(oceanofcode.Point(1, 1), oceanofcode.Point(7, 1))
+        self.assertEqual(len(path), 6)
+
+    def test_not_possible(self):
+        self.m.islands = [oceanofcode.Point(7, y) for y in range(self.m.h)]
+        path = self.m.a_star(oceanofcode.Point(1, 1), oceanofcode.Point(9, 3))
+        self.assertEqual(len(path), 0)
 
 
 class TestNeighbor(unittest.TestCase):
@@ -129,10 +291,21 @@ class TestDirection(unittest.TestCase):
     def test_west(self):
         self.assertEqual(self.m.direction(oceanofcode.Point(1, 1), oceanofcode.Point(0, 1)), 'W')
 
-    def test_error(self):
-        with self.assertRaises(ValueError):
-            self.m.direction(oceanofcode.Point(1, 1), oceanofcode.Point(10, 1))
+    def test_same_position(self):
+        self.assertEqual(self.m.direction(oceanofcode.Point(1, 1), oceanofcode.Point(1, 1)), 'W')
 
+    def test_wide_north(self):
+        self.assertEqual(self.m.direction(oceanofcode.Point(5, 7), oceanofcode.Point(0, 0)), 'N')
+
+class TestSurfaceVoisine(unittest.TestCase):
+    m = oceanofcode.Map(15, 15)
+    def test_nominal(self):
+        self.m.target = oceanofcode.Point(13,9)
+        self.assertEqual(self.m.surface_voisine(), oceanofcode.Point(12,12))
+
+    def test_in_center(self):
+        self.m.target = oceanofcode.Point(7,7)
+        self.assertEqual(self.m.surface_voisine(), oceanofcode.Point(7,2))
 
 if __name__ == '__main__':
     unittest.main()
